5d96a0508f0b2c5ca01221883df38a9317b8b758
Prithwish-2003 <ghoshprithwish586@gmail.com>
2024-03-12 20:19:56 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+setup(
+    name='Sign_Language_Detection',
+    version='0.0.0',
+    author='Prithwish Ghosh',
+    author_email='ghoshprithwish586@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+
+
+)
